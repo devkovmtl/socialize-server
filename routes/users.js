b9f96cb65f3e71e9e7b679a39cf7f203e1ad7f0e
Mikael <dev.kov.mtl@gmail.com>
2022-04-04 11:08:15 -0400

    delete public folder and usres routes

--- routes/users.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const router = Router();
-
-/* GET users listing. */
-router.get('/', (req, res, next) => {
-  res.json({ title: 'User' });
-});
-
-module.exports = router;
