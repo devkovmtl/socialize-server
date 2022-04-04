00d4bedc8e37262ccea21797b1a2a6dc26b55687
Mikael <dev.kov.mtl@gmail.com>
2022-04-03 21:57:03 -0400

    update error handling

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+
+const { NODE_ENV } = process.env;
 const { MONGO_URI, MONGO_OPTIONS } = require('./config/database');
 
 mongoose.connect(MONGO_URI, MONGO_OPTIONS);
@@ -22,8 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/v1/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -32,13 +33,13 @@ app.use((req, res, next) => {
 
 // error handler
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = NODE_ENV === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({
+    success: false,
+    message: err.message || 'Something went wrong',
+    error: NODE_ENV === 'development' ? err : {},
+  });
 });
 
 module.exports = app;
