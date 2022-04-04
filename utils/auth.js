b265b43d0572daa7b342378d35ae253e5e3582ef
Mikael <dev.kov.mtl@gmail.com>
2022-04-04 14:50:07 -0400

    installed new dependencies utils to get user info and create jwt token

--- utils/auth.js
+++ utils/auth.js
@@ -1,16 +1,21 @@
 const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = process.env;
 
+// grab some info from user object to put in token or on the request object
+const userTokenInfo = (user) => ({
+  id: user.id,
+  username: user.username,
+  email: user.email,
+  role: user.role,
+});
+
 // generate a token from payload
-exports.generateJwtToken = (user) => {
-  const payload = {
-    id: user.id,
-    username: user.username,
-    email: user.email,
-    role: user.role,
-  };
+const generateJwtToken = (user) => {
+  const payload = userTokenInfo(user);
   const options = {
-    expiresIn: '1d',
+    expiresIn: '24h',
   };
   return jwt.sign(payload, JWT_SECRET, options);
 };
+
+module.exports = { userTokenInfo, generateJwtToken };
