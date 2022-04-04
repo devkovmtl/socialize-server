f83e283f91eba22ea7738fee2b9637d5903f9f2e
Mikael <dev.kov.mtl@gmail.com>
2022-04-04 10:04:38 -0400

    Added Mongo Database dev

--- config/database.js
+++ config/database.js
@@ -1,14 +1,16 @@
 const {
+  NODE_ENV,
   MONGO_USERNAME = 'admin',
   MONGO_PASSWORD = 'secret',
   MONGO_HOST = 'localhost',
   MONGO_PORT = 27017,
   MONGO_DATABASE = 'social',
+  MONGO_DATABASE_DEV = 'social_dev',
 } = process.env;
 
-// Developement
-// exports.MONGO_URI_DEV = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DATABASE}`;
+const DATABASE =
+  NODE_ENV === 'production' ? MONGO_DATABASE : MONGO_DATABASE_DEV;
 
-exports.MONGO_URI = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}/${MONGO_DATABASE}?retryWrites=true&w=majority`;
+exports.MONGO_URI = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}/${DATABASE}?retryWrites=true&w=majority`;
 
 exports.MONGO_OPTIONS = { useNewUrlParser: true, useUnifiedTopology: true };
