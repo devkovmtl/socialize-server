789edbf33908ade4ac25f4915ae8cadb0c41da69
Mikael <dev.kov.mtl@gmail.com>
2022-04-04 11:10:06 -0400

    setup login and register route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 const { Router } = require('express');
 const router = Router();
 
-/* GET home page. */
-router.get('/', (req, res, next) => {
-  res.json({ title: 'Home' });
+const authController = require('../controllers/authController');
+
+router.get('/', (req, res) => {
+  res.json({
+    success: true,
+    message: 'Welcome to the API',
+  });
 });
+
+/**
+ * Authentication routes
+ */
+router.post('/register', authController.register);
+router.post('/login', authController.login);
 
 module.exports = router;
