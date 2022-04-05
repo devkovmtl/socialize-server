dad44d1aac10a41021e3bb954af015c060fd951b
Mikael <dev.kov.mtl@gmail.com>
2022-04-04 21:37:09 -0400

    added facebook login routes

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = Router();
 const authController = require('../controllers/authController');
 const { isAuthenticated, isAdmin } = require('../middlewares/auth');
 
-router.get('/', isAuthenticated, (req, res) => {
+router.get('/', (req, res) => {
   res.json({
     success: true,
     message: 'Welcome to the API',
@@ -17,4 +17,8 @@ router.get('/', isAuthenticated, (req, res) => {
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.get('/checkAccessToken', authController.checkAccessToken);
+// facebook login
+router.get('/login/facebook', authController.facebookLogin);
+router.get('/redirect/facebook', authController.facebookCallback);
+
 module.exports = router;
