1cdbb8c88d6cd1b60dbfdad03e3f31189534ee9e
Mikael <dev.kov.mtl@gmail.com>
2022-04-05 15:08:40 -0400

    controller to send friend request

---
+++ controllers/friendController.js
@@ -0,0 +1,69 @@
+const { getUserIdFromReq } = require('../utils/getUserIdFromReq');
+const FriendRequest = require('../models/friendRequest');
+
+exports.sendFriendRequest = async (req, res, next) => {
+  const userId = getUserIdFromReq(req);
+  const { friendId } = req.body;
+
+  if (!userId) {
+    return res.status(422).json({
+      success: false,
+      message: 'User id is required',
+      errors: [{ msg: 'User id is required' }],
+    });
+  }
+
+  if (!friendId) {
+    return res.status(422).json({
+      success: false,
+      message: 'Friend id is required',
+      errors: [{ msg: 'Friend id is required' }],
+    });
+  }
+
+  if (userId === friendId) {
+    return res.status(422).json({
+      success: false,
+      message: 'You cannot send friend request to yourself',
+      errors: [{ msg: 'You cannot send friend request to yourself' }],
+    });
+  }
+  try {
+    const areAlreadyFriends = await FriendRequest.findOne({
+      $or: [
+        {
+          $or: [
+            { sender: userId, receiver: friendId, status: 'accepted' },
+            { sender: friendId, receiver: userId, status: 'accepted' },
+          ],
+        },
+        {
+          $or: [
+            { sender: userId, receiver: friendId, status: 'blocked' },
+            { sender: friendId, receiver: userId, status: 'blocked' },
+          ],
+        },
+      ],
+    });
+
+    if (areAlreadyFriends) {
+      return res.status(422).json({
+        success: false,
+        message: 'You are already friends',
+        errors: [{ msg: 'You are already friends' }],
+      });
+    }
+
+    await FriendRequest.create({
+      sender: userId,
+      receiver: friendId,
+    });
+
+    return res.status(201).json({
+      success: true,
+      message: 'Friend request sent successfully',
+    });
+  } catch (error) {
+    next(error);
+  }
+};
